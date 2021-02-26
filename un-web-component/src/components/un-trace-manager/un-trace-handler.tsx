19bd4d0d7cfd2fbe626ac27f605a87d2329c083d
Previn Wong <pwong@esri.com>
2021-02-25 17:14:18 -0800

    initial commit.

---
+++ un-web-component/src/components/un-trace-manager/un-trace-handler.tsx
@@ -0,0 +1,380 @@
+import { Prop} from '@stencil/core';
+import config from '@arcgis/core/config';
+config.assetsPath = 'https://cdn.jsdelivr.net/npm/@arcgis/core@4.18.1/assets';
+import {request} from '@esri/arcgis-rest-request';
+import * as GeometryEngine from '@arcgis/core/geometry/GeometryEngine';
+import * as projection from "@arcgis/core/geometry/projection";
+//import * as Polyline from '@arcgis/core/geometry/Polyline';
+
+export class UnTraceHandler {
+  host="";
+  unName="";
+  constructor(host, name) {
+    this.host = host;
+    this.unName = name;
+  }
+
+  getToken() {
+    return new Promise((resolve: any, reject: any) => {
+      const requestURL = this.host + '/portal/sharing/rest/generateToken';
+      const params = {
+        client_id: 'b97taLpupCPAd4AH',
+        client_secret: '164fbefcfe204fd99fcc2bd8798921d5',
+        grant_type: 'client_credentials',
+        username: 'admin',
+        password: 'esri.agp',
+        referer: 'http://pwonglap.esri.com:3333'
+      };
+
+      //username: 'pwong1',
+      //password: 'pwong1.109',
+
+      this._request({method: 'POST', url:requestURL, params: params})
+      .then((result:any) => {
+        if(result.hasOwnProperty('error')) {
+          resolve(false);
+        } else {
+          resolve(result);
+        }
+      })
+      .catch((e:any) => {
+        resolve(e);
+      });
+    });
+  }
+
+  getTraces(token:string, searchByUser?:string): Promise<any> {
+    return new Promise((resolve: any, reject: any) => {
+      const requestURL = this.host + '/server/rest/services/'+this.unName+'/UtilityNetworkServer/traceConfigurations/query';
+      let params = {};
+      if(token) {
+        params = {f : 'json', globalIds:'', creators:(searchByUser !== '')?'['+ searchByUser + ']':'', tags:'', names:'', token:token};
+      } else {
+        resolve(false);
+      }
+      //requestURL = requestURL + 'VersionManagementServer/versions';
+      this._request({method: 'POST', url:requestURL, params: params})
+      .then((result:any) => {
+        if(result.hasOwnProperty('error')) {
+          resolve(false);
+        } else {
+          resolve(result);
+        }
+      })
+      .catch((e:any) => {
+        resolve(e);
+      });
+    });
+  }
+
+  executeTrace(token: string, traceType:string, flags:Array<any>, traceConfig?:any, traceId?:string): Promise<any> {
+    //traceConfigurationGlobalId:'{80DDAE15-2720-49CA-971F-FBA76AEBC075}'
+    //with barriers
+    //'[{'traceLocationType':'startingPoint','globalId':'{DB331F49-422D-4067-992E-8091D11E479C}','percentAlong':0.5},{'traceLocationType':'barrier','globalId':'{24787450-40BE-44A3-BF1A-2F90630C5DD1}','percentAlong':0.5},{'traceLocationType':'barrier','globalId':'{309C3A4B-CC00-4393-8724-6DA5075BCB16}','percentAlong':0.5},{'traceLocationType':'barrier','globalId':'{074001AE-6A01-4440-A234-9A85C1F93740}','percentAlong':0.5},{'traceLocationType':'barrier','globalId':'{29A7D702-FBB2-46DF-A0FA-99F4D9615BEE}','percentAlong':0.5}]'
+    //just start
+    ////'[{'traceLocationType':'startingPoint','globalId':'{DB331F49-422D-4067-992E-8091D11E479C}','percentAlong':0.5}]'
+    return new Promise((resolve, reject) => {
+      const requestURL = this.host + '/server/rest/services/'+this.unName+'/UtilityNetworkServer/trace';
+      let params = {};
+      if(token) {
+        params = {
+          f : 'json',
+          gdbVersion:'',
+          sessionId:'',
+          moment:'',
+          traceType: traceType,
+          traceLocations: flags,
+          traceConfigurationGlobalId:(typeof traceConfig === 'object' && traceConfig !== null)?'':traceId,
+          traceConfiguration:(traceConfig)?traceConfig:'',
+          token:token
+        };
+      } else {
+        resolve(false);
+      }
+      //requestURL = requestURL + 'VersionManagementServer/versions';
+      this._request({method: 'POST', url:requestURL, params: params})
+      .then(async(result:any) => {
+        if(result.hasOwnProperty('error')) {
+          resolve(false);
+        } else {
+          resolve(result);
+        }
+      })
+      .catch((e:any) => {
+        resolve(e);
+      });
+    });
+  }
+
+  //SUPPORT FUNCTIONS
+  //Switches the parameter for isolation trace so it can be ran twice
+  switchIsoTraceParameter(traceType:string, traceConfig:any) {
+    let updatedConfig = traceConfig;
+    if(traceType === 'isolation') {
+      if (updatedConfig.includeIsolated) {
+        updatedConfig.includeIsolated = false;
+      } else {
+        updatedConfig.includeIsolated = true;
+      }
+    }
+    return updatedConfig;
+  }
+
+  //query for feature to use for various functions such as percentage along and terminal config
+  queryForFeature(token, layer?:any, geom?:any, gdbVersion?:string, moment?:Date): Promise<any> {
+    return new Promise((resolve, reject) => {
+      const requestURL = this.host + '/server/rest/services/'+this.unName+'/FeatureServer/'+ layer.layerId + '/query';
+      let params = {};
+      if(token) {
+        let geomObj: any = '';
+        let geometryType: string = 'esriGeometryPoint';
+        let whereClause:string = '';
+        let objectIds = [];
+        if(layer.ids.length > 0) {
+          objectIds = layer.ids;
+        } else {
+          if(geom) {
+            //line for outage area
+            if(geom.type === "polyline") {
+              geomObj = geom;
+              geometryType = 'esriGeometryPolyline';
+            } else {
+              //it's leak point, buffer it
+              geomObj = this._createBuffer(geom);
+              geometryType = 'esriGeometryPolygon';
+            }
+          }
+          if(layer.subtypes.length > 0) {
+            whereClause = 'assetgroup in (' + layer.subtypes.join(",") + ')';
+          }
+        }
+        params = {
+          f : 'json',
+          objectids: (objectIds.length > 0)?objectIds.join(","):'',
+          where: whereClause,
+          geometry: (objectIds.length === 0)?(geom && geom !== '')?JSON.stringify(geomObj):'':'',
+          geometryType: geometryType,
+          outFields: "*",
+          returnGeometry: true,
+          gdbVersion: (gdbVersion)?gdbVersion:'sde.DEFAULT',
+          historicMoment: (moment)?moment:'',
+          token:token
+        };
+        //console.log(params);
+        //console.log(requestURL);
+      } else {
+        resolve(false);
+      }
+      this._request({method: 'POST', url:requestURL, params: params})
+      .then(async(result:any) => {
+        if(result.hasOwnProperty('error')) {
+          resolve(false);
+        } else {
+          resolve(result);
+        }
+      })
+      .catch((e:any) => {
+        resolve(e);
+      });
+    });
+  }
+
+  // calculate the percentage of where user clicked on the line
+  async getPercentageAlong(sourceGeom:any, flagGeom:any, inSR:any) {
+    let flagLine = flagGeom;
+    const sourceLine = await this._createPolyline(sourceGeom.paths, inSR.wkid);
+    if(flagGeom.type === 'point') {
+      const padFlagXMin = flagGeom.x - 50;
+      const padFlagXMax = flagGeom.x + 50;
+      const padFlagYMin = flagGeom.y - 50;
+      const padFlagYMax = flagGeom.y + 50;
+      const newCoodsForLine = [
+         [padFlagXMin,padFlagYMin],
+         [padFlagXMax,padFlagYMax]
+       ];
+      flagLine = await this._createPolyline(newCoodsForLine, flagGeom.spatialReference.wkid);
+      if(!projection.isLoaded()) {
+        await projection.load();
+      }
+      const projGeom = projection.project(flagLine, inSR);
+      flagLine = projGeom;
+      const newGeom = GeometryEngine.cut(sourceLine,flagLine);
+      if(newGeom.length > 0) {
+        const sourceLength = GeometryEngine.planarLength(sourceLine,'feet');
+        const piece1Length = GeometryEngine.planarLength(newGeom[0],'feet');
+        const percentage = piece1Length / sourceLength;
+        return(percentage);
+      } else {
+        return(0.5);
+      }
+    } else {
+      //it's a line, reproject it
+      if(!projection.isLoaded()) {
+        await projection.load();
+      }
+      const projGeom = projection.project(flagGeom, inSR);
+      flagLine = projGeom;
+      const newGeom = GeometryEngine.cut(sourceLine,flagLine);
+      if(newGeom.length > 0) {
+        const sourceLength = GeometryEngine.planarLength(sourceLine,'feet');
+        const piece1Length = GeometryEngine.planarLength(newGeom[0],'feet');
+        const percentage = piece1Length / sourceLength;
+        return(percentage);
+      } else {
+        return(0.5);
+      }
+    }
+  }
+
+  //create a polyline to use tor percentage along calculation
+  async _createPolyline(geom:any, inSR:any) {
+    //return new Promise(async(resolve, reject) => {
+      const [
+        {default: Polyline}
+      ] = await Promise.all([
+        import('@arcgis/core/geometry/Polyline')
+      ]);
+      const newLine = new Polyline({
+        hasZ: false,
+        hasM: true,
+        paths: geom,
+        spatialReference: { wkid: inSR }
+      });
+      return(newLine);
+    //});
+  }
+
+  //create a buffer for clicked point
+  _createBuffer(geom:any) {
+    return GeometryEngine.buffer(geom, 10, 'feet');
+  }
+
+  //queryDataElement for various uses
+  queryDataElement(token:string) {
+    return new Promise((resolve: any, reject: any) => {
+      const requestURL = this.host + '/server/rest/services/'+this.unName+'/FeatureServer/queryDataElements';
+      let params = {};
+      if(token) {
+        params = {f : 'json', token:token};
+      } else {
+        resolve(false);
+      }
+      //requestURL = requestURL + 'VersionManagementServer/versions';
+      this._request({method: 'POST', url:requestURL, params: params})
+      .then((result:any) => {
+        if(result.hasOwnProperty('error')) {
+          resolve(false);
+        } else {
+          resolve(result);
+        }
+      })
+      .catch((e:any) => {
+        resolve(e);
+      });
+    });
+  }
+
+  findControllerLayer(de:any) {
+    let controller = {};
+    const unLayer = de.layerDataElements.filter((lde:any) => {
+      return lde.dataElement.hasOwnProperty('domainNetworks');
+    });
+    if(unLayer.length > 0) {
+      controller = unLayer[0];
+    }
+    return controller;
+  }
+
+  queryLayersForFlag(controller:any) {
+    let layers = [];
+    const validNetwork = controller.dataElement.domainNetworks.filter((dn:any) => {
+      return dn.subnetworkLayerId !== -1;
+    });
+    if(validNetwork.length > 0) {
+      validNetwork.map((n:any) => {
+        n.edgeSources.map((es:any) => {
+          layers.push(es);
+        });
+        n.junctionSources.map((js:any) => {
+          layers.push(js);
+        });
+      });
+    }
+    return layers;
+  }
+
+  queryATAGTerminals(controller:any) {
+    let terminals = [];
+    const commodities = controller.dataElement.domainNetworks.filter((dn:any) => {
+      return dn.subnetworkLayerId !== -1;
+    });
+    if(commodities.length > 0) {
+      commodities.map((c:any) => {
+        const juncList = this._getTerminalSources(controller, c, "junctionSources");
+        const edgeList = this._getTerminalSources(controller, c, "edgeSources");
+        terminals = terminals.concat(juncList, edgeList);
+      });
+    }
+    return terminals;
+  }
+
+  _getTerminalSources(controller:any, domain:any, sourceType: string) {
+    const sources = [];
+    domain[sourceType].map((js:any) => {
+      js.assetGroups.map((ag:any) => {
+        ag.assetTypes.map((at:any) => {
+          if(at.isTerminalConfigurationSupported) {
+            const termConf = this._getTerminalConfig(controller, at.terminalConfigurationId);
+            sources.push({
+              domainId: domain.domainNetworkId,
+              domain: domain.domainNetworkName,
+              layerId: js.layerId,
+              sourceId: js.sourceId,
+              assetGroupCode: ag.assetGroupCode,
+              assetGroupName: ag.assetGroupName,
+              assetTypeCode: at.assetTypeCode,
+              assetTypeName: at.assetTypeName,
+              terminalConfiguration: termConf
+            });
+          }
+        });
+      });
+    });
+    return sources;
+  }
+
+  _getTerminalConfig(controller: any, terminalId: number) {
+    const termConf = controller.dataElement.terminalConfigurations.filter((tc:any) => {
+      return tc.terminalConfigurationId === terminalId;
+    });
+    if(termConf.length > 0) {
+      return termConf[0];
+    } else {
+      return {};
+    }
+  }
+
+  async projectGeometry(sourceGeom:any, targetSR:any) {
+    if(!projection.isLoaded()) {
+      await projection.load();
+    }
+    const projGeom = projection.project(sourceGeom, targetSR);
+    return projGeom;
+  }
+
+
+  _request(requestObj:any): Promise<any> {
+    return new Promise((resolve: any, reject: any) => {
+      request(requestObj.url, {
+        params: requestObj.params
+      }).then((response:any) => {
+        resolve(response);
+      })
+      .catch((e:any) => {
+        resolve(e);
+      })
+    });
+  }
+
+
+}
---
+++ un-web-component/src/components/un-trace-manager/un-trace-manager.tsx
@@ -0,0 +1,337 @@
+import { Component, Prop, h, State, Event, EventEmitter, Watch} from '@stencil/core';
+import config from '@arcgis/core/config';
+config.assetsPath = 'https://cdn.jsdelivr.net/npm/@arcgis/core@4.18.1/assets';
+import {UnTraceHandler} from "./un-trace-handler";
+import "@esri/calcite-components";
+import * as GeometryEngine from '@arcgis/core/geometry/GeometryEngine';
+import * as projection from "@arcgis/core/geometry/projection";
+
+@Component({
+  tag: 'un-trace-manager',
+  styleUrl: 'un-trace-manager.css',
+  shadow: true,
+})
+export class UnTraceManager {
+  @Prop() host: string = "";
+  @Prop() name: string = "";
+  @Prop() showTerminals: boolean;
+  @Prop() appToken: string = "";
+  @Prop() gdbVersion: string = "sde.DEFAULT";
+  @Prop() inAssets: any;
+  @Prop() inTC: any = {tc:{}, action:"update"};
+  @Prop() runIsoTraceTwice: boolean = true;
+  @Prop() isBasic: boolean = true;
+
+  @Watch('inAssets')
+  watchHandler(newValue: any, oldValue: any, prop:any) {
+    this[prop] = newValue;
+    if(prop === 'inAssets') {this.assetPropsChange();}
+  }
+
+  @Event({eventName: 'emitQueryTrace', composed: true, bubbles: true}) emitQueryTrace: EventEmitter<any>;
+  @Event({eventName: 'emitSelectedTrace', composed: true, bubbles: true}) emitSelectedTrace: EventEmitter<any>;
+  @Event({eventName: 'emitFlagChange', composed: true, bubbles: true}) emitFlagChange: EventEmitter<any>;
+  @Event({eventName: 'emitTraceResults', composed: true, bubbles: true}) emitTraceResults: EventEmitter<any>;
+
+  @State() unHandler: any;
+  @State() token: string;
+  @State() searchByUser: string = "";
+  @State() traceList: Array<any> = [];
+  @State() activeStep: number = 1;
+  @State() activeTrace: any = null;
+  @State() traceResults: any = null;
+  @State() loader: boolean = false;
+  @State() flags: Array<any> = [];
+  //@State() flags: string = '[{"traceLocationType":"startingPoint","globalId":"{DB331F49-422D-4067-992E-8091D11E479C}","percentAlong":0.5}]';
+  //@State() flags: string = '[{"traceLocationType":"startingPoint","globalId":"{DB331F49-422D-4067-992E-8091D11E479C}","percentAlong":0.5},{"traceLocationType":"barrier","globalId":"{24787450-40BE-44A3-BF1A-2F90630C5DD1}","percentAlong":0.5},{"traceLocationType":"barrier","globalId":"{309C3A4B-CC00-4393-8724-6DA5075BCB16}","percentAlong":0.5},{"traceLocationType":"barrier","globalId":"{074001AE-6A01-4440-A234-9A85C1F93740}","percentAlong":0.5},{"traceLocationType":"barrier","globalId":"{29A7D702-FBB2-46DF-A0FA-99F4D9615BEE}","percentAlong":0.5}]';
+  @State() terminals: Array<any> = [];
+  @State() layersForFlagLookup: Array<any> = [];
+  @State() controllerLayer: any;
+
+  componentWillLoad() {
+    this.unHandler = new UnTraceHandler(this.host, this.name);
+    this.unHandler.getToken().then((response:any) => {
+      this.token = response.token;
+
+      this.unHandler.queryDataElement(this.token).then((dataElement:any) => {
+        this.controllerLayer = this.unHandler.findControllerLayer(dataElement);
+        this.layersForFlagLookup = this.unHandler.queryLayersForFlag(this.controllerLayer);
+        this.terminals = this.unHandler.queryATAGTerminals(this.controllerLayer);
+        console.log(this.controllerLayer);
+        console.log(this.layersForFlagLookup);
+        console.log(this.terminals);
+
+        if(this.inAssets) {this.assetPropsChange();}
+
+      });
+
+    });
+  }
+
+  processFlag1(geom:any) {
+    //this.clickPointPropsChange(geom);
+  }
+
+  render() {
+      return(
+        <div style={{display:'flex', flexDirection:'row'}}>
+        <div>
+        <calcite-split-button
+          style={{color:'#000'}}
+          appearance="solid"
+          color="dark"
+          primary-icon-start="search"
+          primary-text="Identify the leak"
+          primary-label="Identify the leak"
+          dropdown-label="Additional Options"
+          dropdown-icon-type="chevron"
+          dir="ltr"
+          calcite-hydrated=""
+          onClick={(evt:any) =>{
+            console.log(evt);
+            console.log('inside component: click flag');
+            this.emitFlagChange.emit({type:'start',tool:'point', callback:this.assetPropsChange});
+          }}
+        >
+          <calcite-dropdown-group
+          style={{color:'#000'}}
+            selection-mode="none"
+            dir="ltr"
+            role="menu"
+            calcite-hydrated=""
+          >
+            <calcite-dropdown-item
+              style={{color:'#000'}}
+              dir="ltr"
+              role="menuitem"
+              selection-mode="none"
+              tabindex="0"
+              calcite-hydrated=""
+              icon-start="point"
+            >
+              Click a point on the map
+            </calcite-dropdown-item>
+            <calcite-dropdown-item
+              style={{color:'#000'}}
+              dir="ltr"
+              role="menuitem"
+              selection-mode="none"
+              tabindex="0"
+              calcite-hydrated=""
+              icon-start="polygonArea"
+            >
+              Draw an area of interest
+            </calcite-dropdown-item>
+            <calcite-dropdown-item
+              style={{color:'#000'}}
+              dir="ltr"
+              role="menuitem"
+              selection-mode="none"
+              tabindex="0"
+              calcite-hydrated=""
+              icon-start="list"
+            >
+              View list of leak locations
+            </calcite-dropdown-item>
+          </calcite-dropdown-group>
+        </calcite-split-button>
+        </div>
+        <div style={{width:"1%"}}></div>
+        <div>
+        <calcite-button
+          appearance="solid"
+          color="blue"
+          scale="m"
+          href=""
+          icon-start="caretRight"
+          dir="ltr"
+          width="auto"
+          calcite-hydrated=""
+        >
+          Run
+        </calcite-button>
+        </div>
+        <div style={{width:"1%"}}></div>
+        <div>
+        <calcite-button
+          appearance="solid"
+          color="light"
+          scale="m"
+          href=""
+          icon-start="xCircle"
+          dir="ltr"
+          width="auto"
+          calcite-hydrated=""
+        >
+          Clear
+        </calcite-button>
+        </div>
+      </div>);
+  }
+
+  renderTraceList(result:any): any {
+    let list = [];
+      result.map((r:any) => {
+        list.push(<calcite-card key={r.name}>
+          <h3 slot="title">{r.name}</h3>
+          <span slot="subtitle">{r.description}</span>
+          <calcite-link slot="footer-leading" onClick={
+            () => {
+              this.activeStep = 3;
+              this.activeTrace = r;
+              this.emitSelectedTrace.emit(r);
+            }
+          }>Select</calcite-link>
+          <calcite-link slot="footer-trailing">Edit Config</calcite-link>
+        </calcite-card>
+        );
+      })
+    return(
+      <div style={{height:"175px", overflow:"auto"}}><div>{list}</div></div>
+    )
+  }
+
+  renderTraceResults(): any {
+    if(this.traceResults !== null) {
+      let list = [];
+      if(this.traceResults.hasOwnProperty("message")) {
+        list.push(<calcite-card>
+          <h3 slot="title">{this.traceResults.message}</h3>
+        </calcite-card>
+        );
+      } else {
+        this.traceResults.traceResults.elements.map((r:any) => {
+          list.push(<calcite-card key={r.globalId}>
+            <h3 slot="title">{r.globalId}</h3>
+            <calcite-link slot="footer-leading">Hightlight</calcite-link>
+          </calcite-card>
+          );
+        })
+      }
+      return(
+        <div style={{height:"175px", overflow:"auto"}}><div>{list}</div></div>
+      )
+    } else {
+      return <div>Error</div>
+    }
+  }
+
+  //Prop change updates
+  assetPropsChange() {
+    if(this.inAssets.length > 0) {
+      console.log(this.inAssets);
+      let assetList = [];
+      this.inAssets.map(async (a:any) => {
+        if(a.layers.length > 0) {
+          a.layers.map((lyr:any) => {
+            assetList.push(this.lookupAsset(lyr, a.geometry));
+          });
+        } else {
+          // only geom is provided.  do lookup
+          this.layersForFlagLookup.map((lyr:any) => {
+            const lyrObj = {layerId: lyr.layerId, ids:[], subtypes:[]};
+            assetList.push(this.lookupAsset(lyrObj, a.geometry));
+          });
+        }
+      });
+      Promise.all(assetList).then((response:any) => {
+        if(response.length > 0) {
+          response.map((res:any) => {
+            if(res.result.features.length > 0) {
+              console.log(res);
+              res.result.features.map((feat:any) => {
+                if(res.result.geometryType === 'esriGeometryPolyline') {
+                  //get percent along
+                  this.getPercentAlong(feat.geometry,  res.flagGeom, res.result.spatialReference).then((result:any) => {
+                    const flagExists = this.flags.indexOf((f:any) => {
+                      return f.globalId === feat.attributes.globalid;
+                    });
+                    if(flagExists === -1) {
+                      this.flags.push(
+                        {"traceLocationType":"start","globalId": feat.attributes.globalid, "percentAlong":result}
+                      );
+                    }
+                  });
+                  //if line on line, send back the intersected point for flag graphic
+                  if(res.flagGeom.type === "polyline") {
+                    this.intersectToPoint(feat.geometry, res.flagGeom, res.result.spatialReference).then((point:any) => {
+                      console.log("line/line to point");
+                      console.log(point);
+                    });
+                  }
+                } else if(res.result.geometryType === 'esriGeometryPoint') {
+                  //get terminals
+                  const terminalList = this.terminals.filter((t:any) => {
+                    return (
+                      t.assetGroupCode === feat.attributes.assetgroup &&
+                      t.assetTypeCode === feat.attributes.assettype &&
+                      t.layerId === res.layerId
+                    )
+                  });
+                  if(terminalList.length > 0) {
+                    const flagExists = this.flags.indexOf((f:any) => {
+                      return f.globalId === feat.attributes.globalid;
+                    });
+                    if(flagExists === -1) {
+                      this.flags.push(
+                        {"traceLocationType":"start", "globalId": feat.attributes.globalid, "terminal":terminalList[0].terminalConfiguration.terminals[0], "allTerminals":terminalList[0].terminalConfiguration}
+                      );
+                    }
+                  }
+                } else {
+                  //do nothing, it might be a polygon or invalid type
+                }
+              });
+            }
+          });
+          console.log(this.flags);
+        }
+      });
+    }
+  }
+
+  //SUPPORT FUNCTIONS
+  lookupAsset(lyr?:any, geom?: any): Promise<any> {
+    return new Promise(async(resolve, reject) => {
+      let geomObj = geom;
+      if(geom.type === "polygon") {
+        //convert it to polyline and reproject it.
+        const rings = geom.rings;
+        geomObj = await this.unHandler._createPolyline(rings, geom.spatialReference.wkid);
+      } else {
+        //polyline and point, just reproject
+      }
+      this.unHandler.queryForFeature(this.token, lyr, geomObj).then((response:any)=> {
+        //console.log(response);
+        const resObj = {
+          "result": response,
+          "flagGeom": geomObj,
+          "layerId": lyr.layerId
+        }
+        resolve(resObj);
+      })
+      .catch((e: any) => {
+        resolve(e);
+      });
+    });
+  }
+
+  getPercentAlong(geometry:any, flagGeom: any, sr:any) {
+    return new Promise((resolve, reject) => {
+      this.unHandler.getPercentageAlong(geometry,  flagGeom, sr).then((response:any) => {
+        resolve(response);
+      })
+      .catch((e: any) => {
+        resolve(e);
+      });
+    });
+  }
+
+  async intersectToPoint(geometry:any, flagGeom: any, sourceSR:any) {
+    const sourceLine = await this.unHandler._createPolyline(geometry.paths, sourceSR.wkid);
+    const flagToProjected = await this.unHandler.projectGeometry(flagGeom, sourceSR);
+    const newGeom = GeometryEngine.intersect(sourceLine,flagToProjected);
+    return newGeom;
+  }
+
+}
